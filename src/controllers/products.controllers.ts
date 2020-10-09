328610b6225b79af5052bd9fa7bb751fc2f0d7d2
Brayan Castillo <bhcastillor@gmail.com>
2020-10-08 22:30:40 -0500

    add functionality product available and bestSeller

--- src/controllers/products.controllers.ts
+++ src/controllers/products.controllers.ts
@@ -1,16 +1,19 @@
 import { Request, Response } from 'express';
 import { connect } from '../database';
 import { IPurchase } from 'models/customer';
-import { validateCustomerId } from '../middlewares/validatePurchase';
 
 export const getAll = async (req: Request, res: Response): Promise<Response> => {
   const conn = await connect();
   const categories = await conn.query('SELECT * FROM categories');
-  let productsQuery = await conn.query(`
+  const bestSellerId = await bestSellingProduct(conn);
+  const productsQuery = await conn.query(`
   SELECT 
     products.id, 
     products.name, 
     concat('[', GROUP_CONCAT(categories.id ORDER BY categories.id ASC SEPARATOR ', ') , ']') AS categories, 
+    NULL AS available,
+    NULL AS bestSeller, 
+    products.quantity,
     products.price,
     products.img,
     products.description
@@ -18,10 +21,12 @@ export const getAll = async (req: Request, res: Response): Promise<Response> => 
   INNER JOIN categoriesproducts ON categories.id = categoriesproducts.category_id
   INNER JOIN products ON categoriesproducts.product_id = products.id
   GROUP BY products.id`);
-  let productsStringify = JSON.stringify(productsQuery[0]);
-  let products = JSON.parse(productsStringify);
+  const products = JSON.parse(JSON.stringify(productsQuery[0]));
   products.forEach((product: any) => {
     product.categories = JSON.parse(product.categories);
+    product.available = product.quantity > 0 ? true : false;
+    product.bestSeller = bestSellerId === product.id ? true : false;
+    delete product.quantity;
   });
 
   const query = { categories: categories[0], products: products };
@@ -37,4 +42,15 @@ export const createPurchase = async (req: Request, res: Response): Promise<Respo
           VALUES (NULL, ${customerId}, ${product.id}, ${product.quantity}, '2020-10-07')`);
   });
   return res.status(400).json({ message: 'purchase completed' });
+};
+
+const bestSellingProduct = async (conn: any) => {
+  const bestSellerQuery = await conn.query(`
+    SELECT product_id as idProductBestSeller, SUM(quantity)as bestSeller 
+    FROM sold_products 
+    GROUP BY product_id 
+    ORDER BY bestSeller DESC 
+    LIMIT 1`);
+  const bestSeller = JSON.parse(JSON.stringify(bestSellerQuery[0]));
+  return bestSeller[0].idProductBestSeller;
 };
