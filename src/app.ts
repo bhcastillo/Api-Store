9ac3346fea5a9ee1a4c3e72d6cdd7a845da45dbb
Brayan Castillo <bhcastillor@gmail.com>
2020-10-09 12:08:44 -0500

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { Application } from 'express';
+import  cors  from 'cors';
+
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 //Routes
@@ -8,6 +10,7 @@ const app: Application = express();
 //Settings
 app.set('PORT', 3000);
 //Middlewares
+app.use(cors())
 app.use(express.json());
 app.use(morgan('dev'));
 //Routes
