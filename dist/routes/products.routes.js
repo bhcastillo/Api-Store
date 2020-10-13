d46abb68e5e8eae0c99803ab520f1c44a755cee1
Brayan Castillo <bhcastillor@gmail.com>
2020-10-12 23:52:36 -0500

    change for heroku1

--- dist/routes/products.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const products_controllers_1 = require("../middlewares/products.controllers");
-const router = express_1.Router();
-router.get('/products', products_controllers_1.getProducts);
-router.get('/product/:id', products_controllers_1.getProduct);
-exports.default = router;
-//# sourceMappingURL=products.routes.js.map
