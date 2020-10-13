8bc8e807dbd5336a6b8a63cf8518ccc8f3264829
Brayan Castillo <bhcastillor@gmail.com>
2020-10-12 23:46:21 -0500

    change for heroku

--- dist/database.js
+++ dist/database.js
@@ -17,7 +17,6 @@ exports.connect = () => __awaiter(void 0, void 0, void 0, function* () {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        connectionLimit: 5000,
     });
 });
 //# sourceMappingURL=database.js.map
